494febeff2e20f8d22659d86a57d54d9d089f55f
kelvine03 <akladeks@espacelaparole.com>
2019-03-15 00:07:42 +0000

    page de splashScreen et slide de bienvenu ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:bedoubank/pages/SlideAccueil.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  
+void main() => runApp(new MonApp());
+
+class MonApp extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+    return new MaterialApp(
+      home: new SplashScreen(),
+      theme: new ThemeData(
+          primarySwatch: Colors.blue
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/SlideAccueil': (BuildContext context) => new SlideAccueil()
+      },
+
+
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class SplashScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SplashScreenState createState() => new _SplashScreenState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationPage);
   }
-
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/SlideAccueil');
+  }
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
   @override
   Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-      
-        title: Text(widget.title),
+    return new Scaffold(
+   backgroundColor: Color(0xff11172A),
+      body: new Center(
+        child: new Image.asset('images/logo.png'),
       ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/pages/SlideAccueil.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+import 'package:intro_slider/intro_slider.dart';
+
+class SlideAccueil extends StatefulWidget {
+  SlideAccueil({Key key}) : super(key: key);
+
+  @override
+  SlideAccueilState createState() => new SlideAccueilState();
+}
+
+// Custom config
+class SlideAccueilState extends State<SlideAccueil> {
+  List<Slide> slides = new List();
+
+  @override
+  void initState() {
+    super.initState();
+
+    slides.add(
+      new Slide(
+        title:
+            "TRANSFERT D'ARGENT",
+        maxLineTitle: 2,
+        styleTitle: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            "DESCRIPTION ICI,",
+        styleDescription: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        marginDescription:
+            EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 70.0),
+        centerWidget: Text("Replace this with a custom widget",
+            style: TextStyle(color: Colors.white)),
+
+        pathImage: "images/logo_SGBCI.png",
+        backgroundColor: Color(0xff11172A),
+        directionColorBegin: Alignment.topLeft,
+        directionColorEnd: Alignment.bottomRight,
+        onCenterItemPress: () {},
+      ),
+    );
+    slides.add(
+      new Slide(
+        title: "RETRAIT D'ARGENT",
+        styleTitle: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            "Faite vos retrait dans tous nos guiches",
+        styleDescription: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        pathImage: "images/loginx.png",
+        backgroundColor: Color(0xff11172A),
+        directionColorBegin: Alignment.topRight,
+        directionColorEnd: Alignment.bottomLeft,
+      ),
+    );
+    slides.add(
+      new Slide(
+        title: "Payement de facture",
+        styleTitle: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoMono'),
+        description:
+            "Payez tous vos facture avec bedoubank",
+        styleDescription: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontStyle: FontStyle.italic,
+            fontFamily: 'Raleway'),
+        pathImage: "images/userandroid.png",
+        backgroundColor: Color(0xff11172A),
+        directionColorBegin: Alignment.topCenter,
+        directionColorEnd: Alignment.bottomCenter,
+        maxLineTextDescription: 3,
+      ),
+    );
+  }
+
+  void onDonePress() {
+    // TODO: go to next screen
+  }
+
+  void onSkipPress() {
+    // TODO: go to next screen
+  }
+
+  Widget renderNextBtn() {
+    return Icon(
+      Icons.navigate_next,
+      color: Colors.white,
+      size: 35.0,
+    );
+  }
+
+  Widget renderDoneBtn() {
+    return Icon(
+      Icons.done,
+      color: Colors.white,
+    );
+  }
+
+  Widget renderSkipBtn() {
+    return Icon(
+      Icons.skip_next,
+      color: Colors.white,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new IntroSlider(
+      // List slides
+      slides: this.slides,
+
+      // Skip button
+      renderSkipBtn: this.renderSkipBtn(),
+      onSkipPress: this.onSkipPress,
+      colorSkipBtn: Color(0x33000000),
+      highlightColorSkipBtn: Color(0xff000000),
+
+      // Next button
+      renderNextBtn: this.renderNextBtn(),
+
+      // Done button
+      renderDoneBtn: this.renderDoneBtn(),
+      onDonePress: this.onDonePress,
+      colorDoneBtn: Color(0x33000000),
+      highlightColorDoneBtn: Color(0xff000000),
+
+      // Dot indicator
+      colorDot: Color(0x33D02090),
+      colorActiveDot: Colors.white,
+      sizeDot: 13.0,
+
+      // Locale
+      locale: 'en',
+
+      // Show or hide status bar
+      shouldHideStatusBar: true,
+
+      // Change slides only with buttons
+      isScrollable: false,
+    );
+  }
+}
